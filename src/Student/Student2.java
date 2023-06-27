08f68db991f72e2dacfd9998c2c016c845b01dc6
JazeeraArinova <canarijaz@gmail.com>
2023-06-26 20:50:07 -0400

    Created Student2 with 'getInfo' method

---
+++ src/Student/Student2.java
@@ -0,0 +1,11 @@
+package Student;
+
+public class Student2 {
+    public static void main(String[] args) {
+        getInfo("Jazeera Arinova");
+
+    }
+    public static String getInfo(String name){
+        return name;
+    }
+}
