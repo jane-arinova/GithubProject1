0e025aa0179a56c851d2c376b9950109d366a430
JazeeraArinova <canarijaz@gmail.com>
2023-06-26 20:48:49 -0400

    Created Student1

---
+++ src/Student/Student1.java
@@ -0,0 +1,13 @@
+package Student;
+
+public class Student1 {
+    public static void main(String[] args) {
+        int n =4;
+        int m =8;
+        if(n>m){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
